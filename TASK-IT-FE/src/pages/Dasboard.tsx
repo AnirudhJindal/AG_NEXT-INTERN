895a17861fe9a77980c08fd2b3b8d011cbee692c
AnirudhJindal <150540550+AnirudhJindal@users.noreply.github.com>
2026-01-15 17:46:30 +0530

    Update Dasboard.tsx

--- TASK-IT-FE/src/pages/Dasboard.tsx
+++ TASK-IT-FE/src/pages/Dasboard.tsx
@@ -38,7 +38,7 @@ export const Dashboard = () => {
           return
         }
 
-        const res = await axios.get('${import.meta.env.VITE_API_URL}/api/task', {
+        const res = await axios.get('https://localhost:3000/signin/api/task', {
           headers: { Authorization: `${token}` },
         })
 //@ts-ignore
