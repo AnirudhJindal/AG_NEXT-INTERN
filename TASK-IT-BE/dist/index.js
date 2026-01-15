20e64a10cb30d19b5bb9bc9b42f0af043dd9df56
Anirudh Jindal <jindalanirudh0@gmail.com>
2026-01-15 15:32:15 +0530

    ok

--- TASK-IT-BE/dist/index.js
+++ TASK-IT-BE/dist/index.js
@@ -8,13 +8,10 @@ const user_1 = require("./routers/user");
 const cors_1 = __importDefault(require("cors"));
 const auth_1 = require("./routers/auth");
 const mongoose_1 = __importDefault(require("mongoose"));
-mongoose_1.default.connect("mongodb+srv://ANIRUDH:q543H7zVHKujxL9h@cluster0.lbiokv1.mongodb.net/TaskIt");
+mongoose_1.default.connect(process.env.MONGO_URI);
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 app.use("/api/auth", auth_1.authRouter);
 app.use("/api/task", user_1.taskRouter);
-app.listen(3000, () => {
-    console.log("app has started on port 3000");
-});
 exports.default = app;
